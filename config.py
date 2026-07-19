c501380e3e460528ea039967cd7b6a3555a234d3
luch978 <luch978@gmail.com>
2026-07-19 12:55:43 +0500

    Add files via upload

--- config.py
+++ config.py
@@ -1,6 +1,20 @@
 ﻿TOKEN = "7704135885:AAHU3UbcYckiwq1iVxzWLtlUeFQFwi45gWM"
-CHAT_ID = "52119049"  # ИСПРАВЛЕНО: теперь строка
-BASE = "https://fapi.binance.com"
+CHAT_ID = "52119049"
+
+# Конфиги бирж
+EXCHANGES = {
+    "binance": {
+        "base": "https://fapi.binance.com",
+        "ws": "wss://fstream.binance.com/ws",
+        "name": "Binance"
+    },
+    "mexc": {
+        "base": "https://api.mexc.com",
+        "ws": "wss://wbs.mexc.com/ws",
+        "name": "MEXC"
+    }
+}
+
 PROXY_LIST = [
     "http://z8Nfpp:87j2oy@185.240.94.115:8000",
 ]
